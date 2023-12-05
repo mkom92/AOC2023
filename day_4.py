b50bc19827010f61596b505447f1ddad5449714d
mkom92 <mikolaj.komorowski92@gmail.com>
2023-12-05 18:28:13 +0100

    removed unnecessary code

--- day_4.py
+++ day_4.py
@@ -22,16 +22,6 @@ def matches(elf_numbers_list, winning_numbers_list) -> list:
 	return [star_1, star_2]
 
 
-def add_occurence(dict, card, add_coeficient):
-
-	try:
-		dict[card] += 1
-	except:
-		dict[card] = 1
-
-	return dict
-
-
 def star(file_name, star_num) -> int:
 
 	result = 0
@@ -61,16 +51,12 @@ def star(file_name, star_num) -> int:
 
 				matching_numbers = matches(elf_numbers_list, winning_numbers_list)[1]
 
-				# card_occurrences = add_occurence(card_occurrences, card)
-
 				try:
 					card_occurrences[card] += 1
 				except:
 					card_occurrences[card] = 1
 
 				for i in range(matching_numbers):
-
-					# card_occurrences = add_occurence(card_occurrences, int(card) + i + 1)
 
 					try:
 						card_occurrences[card + i + 1] += 1 * card_occurrences[card]
