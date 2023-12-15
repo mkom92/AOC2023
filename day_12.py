c0aec1e71b4447f672fa1ee93db7a6f6c219607f
mkom92 <mikolaj.komorowski92@gmail.com>
2023-12-15 15:09:21 +0100

    Advent of code - day 12 solution - remade from scratch

--- day_12.py
+++ day_12.py
@@ -1,74 +1,115 @@
+import functools
 import re
-from sympy.utilities.iterables import multiset_permutations
-from json import dumps
+from util import timeit
 
-def star(file_name: str, star_num: int) -> int:
+@functools.lru_cache(maxsize=None)
+def decoding(instruction: str, parts_map: tuple):
+
+	if parts_map == ():
+
+		if instruction.count('#') == 0:
+
+			return 1
+
+		else:
+
+			return 0
+
+	elif instruction == '':
+
+		return 0 
+
+	elif instruction.count('#') + instruction.count('?') < sum(parts_map):
+
+		return 0
+
+	char = instruction[0]
+	dmg_group = parts_map[0]
+
+
+	# def functions for # 
+
+	def hashtag(instruction: str, parts_map: tuple):
+
+		dmg_group = parts_map[0]
+
+		test_dmg = '#'*dmg_group
+		test_vs = re.sub(r'\?','#',instruction[:dmg_group])
+
+		try:
+			next_char_test = instruction[dmg_group] != '#'
+			is_end = False
+		except:
+			is_end = True
+
+
+		if is_end:
+			return 1
+
+		else:
+
+			if test_vs == test_dmg and next_char_test:
+
+				return decoding(instruction[dmg_group+1:], parts_map[1:])
+
+			else:
+
+				return 0
+
+
+	if char == '#':
+
+		return hashtag(instruction, parts_map)
+
+	elif char == '.':
+
+		return decoding(instruction[1:], parts_map)
+
+	elif char == '?':
+
+		return hashtag(instruction, parts_map) + decoding(instruction[1:], parts_map)
+
+
+@timeit
+def star_1(file_name: str) -> int:
+
 
 	with open(file_name, 'r') as f:
-		parts_records = {}
 
-		p_damaged  = re.compile("\#+")
-		p_unknown  = re.compile("\?")
+		inp = [[i for i in line.strip().split(' ')] for line in f.readlines()]
+	
+	total_val = 0
+	for i in inp:
 
-		for n,line in enumerate(f.readlines()):
+		val = 0
+		instruction, parts_map = i[0], tuple(int(j) for j in i[1].split(','))
 
-			newline = [i for i in line.strip().split()]
-			parts_records[n] = {}
-			parts_records[n]['instruction'] = newline[0]
-			parts_records[n]['instruction_next'] = newline[0]+ '?' + newline[0]
-			parts_records[n]['parts_map'] = [int(i) for i in newline[1].split(',')]
-			# parts_records[n]['damaged_pos'] = [(m.start(), m.start() + len(m.group()) - 1) for m in p_damaged.finditer(newline[0])]
-			parts_records[n]['unknown_pos'] = [int(m.start()) for m in p_unknown.finditer(newline[0])]
-			parts_records[n]['unknown_pos_next'] = [int(m.start()) for m in p_unknown.finditer(newline[0]+'?'+newline[0])]
+		val = decoding(instruction, parts_map)
+		total_val += val
 
-	total_ct, total_ct_next = 0,0
-	for n, line in parts_records.items():
-
-		unknowns = len(line['unknown_pos'])
-		damaged =  sum([len(m.group()) for m in p_damaged.finditer(line['instruction'])])
-		max_damaged = sum(line['parts_map'])
-
-		inp_string = '#'*(max_damaged-damaged) + '.'*(unknowns-(max_damaged-damaged))
-		sub_ct = 0
-
-		for i in multiset_permutations(inp_string):
-			new_line = line['instruction']
-			for m,pos in enumerate(line['unknown_pos']):
-
-				new_line = new_line[:pos] + i[m] + new_line[pos+1:]
-
-			if [len(m.group()) for m in p_damaged.finditer(new_line)] == line['parts_map']:
-				sub_ct += 1
-
-		parts_records[n]['match_ct'] = sub_ct
-		total_ct += sub_ct
-
-		unknowns_next = len(line['unknown_pos_next'])
-		inp_string_next = '#'*(max_damaged-damaged)*2 + '.'*(unknowns_next-((max_damaged-damaged)*2))
-		sub_ct_next = 0
-
-		for i in multiset_permutations(inp_string_next):
-			new_line = line['instruction_next']
-			for m,pos in enumerate(line['unknown_pos_next']):
-
-				new_line = new_line[:pos] + i[m] + new_line[pos+1:]
-
-			if [len(m.group()) for m in p_damaged.finditer(new_line)] == line['parts_map']*2:
-				sub_ct_next += 1
-
-		parts_records[n]['match_ct_next'] = sub_ct_next
-		total_ct_next += (sub_ct_next/sub_ct)**4 * sub_ct
-
-		sub_ct = 0
-		sub_ct_next = 0
+	return total_val
 
 
-	# print(dumps(parts_records, indent=4))
-	if star_num == 1:
-		return total_ct 
-	elif star_num == 2:
-		return total_ct_next
+@timeit
+def star_2(file_name: str) -> int:
+
+
+	with open(file_name, 'r') as f:
+
+		inp = [[i for i in line.strip().split(' ')] for line in f.readlines()]
+	
+	total_val = 0
+	for i in inp:
+
+		val = 0
+		instruction, parts_map = '?'.join([i[0] for _ in range(5)]), tuple(int(j) for j in i[1].split(',')) * 5
+		val = decoding(instruction, parts_map)
+		total_val += val
+
+	return total_val
+
+
 
 if __name__ == "__main__":
-	print(f"Star 1: {star('day_12.txt',1)}")
-	# print(f"Star 2: {star('day_12.txt',2)}")
+	print(f"Star 1: {star_1('day_12.txt')}")
+	print(f"Star 2: {star_2('day_12.txt')}")
